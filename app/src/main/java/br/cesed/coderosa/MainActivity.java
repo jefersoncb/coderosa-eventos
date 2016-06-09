0ec92d0c312269a1adc59139e56cee05f244b055
jefersoncb <jefersoncostab@gmail.com>
2016-06-09 20:34:13 -0300

    Correcoes titlebar

--- app/src/main/java/br/cesed/coderosa/MainActivity.java
+++ app/src/main/java/br/cesed/coderosa/MainActivity.java
@@ -69,10 +69,10 @@ public class MainActivity extends AppCompatActivity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         if (id == R.id.nav_sobre) {
             Intent intent = new Intent(this, activitySobre.class);
